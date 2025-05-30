bea5bca2b6c434d542fab8a5c8107a5dcbfdc729
md <maciek.dudek@atkins.cloud.dk>
2025-05-30 09:04:13 +0000

    update script

--- .github/scripts/external_publish_after.sh
+++ .github/scripts/external_publish_after.sh
@@ -5,4 +5,5 @@ source "$(dirname "$0")/helpers/git-utils.sh"
 
 echo "🧹 Cleaning up publish branch..."
 run_git "deleting remote branch $PUBLISH_BRANCH" push origin --delete "$PUBLISH_BRANCH" || true
-run_git "deleting remote tag $PUBLISH_BRANCH" push origin --delete "v-$PUBLISH_BRANCH" || true
+TAG_TO_DELETE="${PUBLISH_BRANCH#publish-}"
+run_git "deleting remote tag $TAG_TO_DELETE" push origin --delete "v-$TAG_TO_DELETE" || true
